3b4ad879331dcfc3ff1f048529fc1d216e73e7f4
lsrain <315192042@qq.com>
2016-12-07 02:01:07 +0800

    project init

---
+++ LSDrawRectLineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSDrawRectLineDemo
+//
+//  Created by WangBiao on 2016/12/7.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
